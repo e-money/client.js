dd0ea680fbfba9a969fb78338ae0d9d791a617cc
Martin Dyring-Andersen <martin@dyring-andersen.dk>
2021-09-29 20:34:02 +0200

    Set default memo values

--- src/signingemoneyclient.ts
+++ src/signingemoneyclient.ts
@@ -51,7 +51,7 @@ export class SigningEmoneyClient extends SigningStargateClient {
   // A limit order specifies the limit (worst) price to trade at. When the order is filled it might be filled at a better price (receive "price improvement").
   // The limit price is calculated as: Price = Destination / Source.
   public async addLimitOrder (ownerAddress: string, clientOrderId: string, timeInForce: TimeInForce,
-    source: Coin, destination: Coin, fee: StdFee, memo: string = '') : Promise<BroadcastTxResponse> {
+    source: Coin, destination: Coin, fee: StdFee, memo: string | undefined = undefined) : Promise<BroadcastTxResponse> {
     const addLimitOrderMsg: MsgAddLimitOrderEncodeObject = {
       typeUrl: '/em.market.v1.MsgAddLimitOrder',
       value: {
@@ -69,7 +69,7 @@ export class SigningEmoneyClient extends SigningStargateClient {
   // Market orders are converted to limit orders on receipt: The limit price is determined using the last traded price of its instrument,
   // with a slippage value applied to determine the limit price. A market order will be rejected in case the instrument has not been traded yet.
   public async addMarketOrder (ownerAddress: string, clientOrderId: string, timeInForce: TimeInForce,
-    source: string, destination: Coin, maximumSlippage: number, fee: StdFee, memo: string = '') : Promise<BroadcastTxResponse> {
+    source: string, destination: Coin, maximumSlippage: number, fee: StdFee, memo: string | undefined = undefined) : Promise<BroadcastTxResponse> {
     const addMarketOrderMsg: MsgAddMarketOrderEncodeObject = {
       typeUrl: '/em.market.v1.MsgAddMarketOrder',
       value: {
@@ -86,7 +86,7 @@ export class SigningEmoneyClient extends SigningStargateClient {
 
   // See https://github.com/e-money/em-ledger/blob/master/x/market/spec/02_messages.md#msgcancelorder
   // The unfilled part of an active order can be canceled using MsgCancelOrder.
-  public async cancelOrder (ownerAddress: string, clientOrderId: string, fee: StdFee, memo: string = '') : Promise<BroadcastTxResponse> {
+  public async cancelOrder (ownerAddress: string, clientOrderId: string, fee: StdFee, memo: string | undefined = undefined) : Promise<BroadcastTxResponse> {
     const cancelOrderMsg: MsgCancelOrderEncodeObject = {
       typeUrl: '/em.market.v1.MsgCancelOrder',
       value: {
@@ -100,7 +100,7 @@ export class SigningEmoneyClient extends SigningStargateClient {
   // See https://github.com/e-money/em-ledger/blob/master/x/market/spec/02_messages.md#msgcancelreplacelimitorder
   // The MsgCancelReplaceLimitOrder message is useful for liquidity providers (market makers) who wish to adjust their prices while remaining in the market.
   public async cancelReplaceLimitOrder (ownerAddress: string, originalClientOrderId: string, newClientOrderId: string, timeInForce: TimeInForce,
-    source: Coin, destination: Coin, fee: StdFee, memo: string = '') : Promise<BroadcastTxResponse> {
+    source: Coin, destination: Coin, fee: StdFee, memo: string | undefined = undefined) : Promise<BroadcastTxResponse> {
     const cancelReplaceLimitOrderMsg: MsgCancelReplaceLimitOrderEncodeObject = {
       typeUrl: '/em.market.v1.MsgCancelReplaceLimitOrder',
       value: {
@@ -120,7 +120,7 @@ export class SigningEmoneyClient extends SigningStargateClient {
   // Please note, The new Market order is converted to limit order on receipt: The limit price is determined using the last traded price of its instrument,
   // with the slippage value applied resulting in the updated limit price.
   public async cancelReplaceMarketOrder (ownerAddress: string, originalClientOrderId: string, newClientOrderId: string, timeInForce: TimeInForce,
-    source: string, destination: Coin, maximumSlippage: number, fee: StdFee, memo: string = '') : Promise<BroadcastTxResponse> {
+    source: string, destination: Coin, maximumSlippage: number, fee: StdFee, memo: string | undefined = undefined) : Promise<BroadcastTxResponse> {
     const cancelReplaceMarketOrderMsg: MsgCancelReplaceMarketOrderEncodeObject = {
       typeUrl: '/em.market.v1.MsgCancelReplaceMarketOrder',
       value: {
